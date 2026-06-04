d3cdc99242753fd4c2383a1344fe339b253c1c3d
Milkftw <282414362+Milkftw@users.noreply.github.com>
2026-06-04 16:01:41 -0700

    Add token usage output and metadata check

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import os 
+from dotenv import load_dotenv
+from google import genai
+
+
+
 def main():
-    print("Hello from milk-ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("The api_key is missing. Please check if it was entered correctly.")
+    client = genai.Client(api_key=api_key) 
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata not found - likely a failed API request.")
+    print(
+        f"Prompt tokens: {response.usage_metadata.prompt_token_count}\n"
+        f"Response tokens: {response.usage_metadata.candidates_token_count}"
+    )
+    print(f"Response:\n{response.text}") 
+
 
 
 if __name__ == "__main__":
